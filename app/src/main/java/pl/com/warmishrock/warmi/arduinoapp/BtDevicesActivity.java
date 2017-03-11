8329abe96c6acff6dc54bc4c361e2b556764e5cb
Warmish <warmishrock@gmail.com>
2017-03-11 21:03:58 +0100

    New Classes, Custom adapter for bluetooth devices, some minor fixes

--- app/src/main/java/pl/com/warmishrock/warmi/arduinoapp/BtDevicesActivity.java
+++ app/src/main/java/pl/com/warmishrock/warmi/arduinoapp/BtDevicesActivity.java
@@ -1,14 +1,83 @@
 package pl.com.warmishrock.warmi.arduinoapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class BtDevicesActivity extends AppCompatActivity
 {
+    public static int howManyPairedDevices;
+    private ListView list ;
+    public String names;
+    public String macs;
+    List<String> deviceName = new ArrayList<String>();
+    List<String> deviceMac = new ArrayList<String>();
+    private static final byte REQUEST_ENABLE_BT = 1;
+
+    public BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+    public void checkIfBtOn() {
+        if (mBluetoothAdapter == null)
+        {
+            Toast.makeText(getApplicationContext(), "Nie wykryto modułu!", Toast.LENGTH_SHORT).show();
+        }
+        else
+        if (!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Bluetooth jest już włączony!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_bluetooth_device);
+        checkIfBtOn();
+
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                deviceName.add(device.getName());
+                deviceMac.add(device.getAddress());
+            }
+        }
+
+        if (howManyPairedDevices > 0) {
+            for (int i = 0; i < howManyPairedDevices; ++i)
+            {
+                //devices = new Devices[deviceName.get(i),deviceMac.get(i)];
+            }
+        }
+
+        Devices devices[] = new Devices[]{
+                new Devices(deviceName.get(0), deviceMac.get(0)),
+                new Devices(deviceName.get(1), deviceMac.get(1)),
+                new Devices(deviceName.get(2), deviceMac.get(2)),
+                new Devices(deviceName.get(3), deviceMac.get(3)),
+                new Devices(deviceName.get(4), deviceMac.get(4)),
+                new Devices(deviceName.get(5), deviceMac.get(5)),
+                new Devices(deviceName.get(6), deviceMac.get(6)),
+                new Devices("2. ", "BMW"),
+
+        };
+
+        DeviceAdapter adapter = new DeviceAdapter(this, R.layout.bluetooth_devices, devices);
+
+        list = (ListView) findViewById(R.id.deviceLV);
+        list.setAdapter(adapter);
     }
 }
--- app/src/main/java/pl/com/warmishrock/warmi/arduinoapp/MainActivity.java
+++ app/src/main/java/pl/com/warmishrock/warmi/arduinoapp/MainActivity.java
@@ -29,12 +29,10 @@ public class MainActivity extends AppCompatActivity {
     TextView textView1;
     FloatingActionButton fab;
     Button button;
-    ArrayList<String> nazwa;
-    ArrayList<String> macaddress;
     private static final byte REQUEST_ENABLE_BT = 1;
 
-    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+    public BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,45 +40,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         addComponents();
         setSupportActionBar(toolbar);
+        setButtonsListener();
+    }
+
+    public void setButtonsListener() {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        //.setAction("Action", null).show();
-                if (mBluetoothAdapter == null) {
-                    Toast.makeText(getApplicationContext(), "Nie wykryt modułu!", Toast.LENGTH_SHORT).show();
-                }
-                else
-                if (!mBluetoothAdapter.isEnabled()) {
-                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-                } else {
-                    Toast.makeText(getApplicationContext(), "Bluetooth jest już włączony!", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(getApplicationContext(), "Wysłano polecenie!", Toast.LENGTH_SHORT).show();
             }
         });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    if (pairedDevices.size() > 0) {
-                        // There are paired devices. Get the name and address of each paired device.
-                        for (BluetoothDevice device : pairedDevices) {
-                            nazwa.add(device.getName());
-                            macaddress.add(device.getAddress()); // MAC address
 
-
-                        }
-                    }
-
-                Intent intent = new Intent(MainActivity.this, BtDevicesActivity.class);
-                intent.putExtra("nazwa", nazwa);
-
-                intent.putExtra("macaddress", macaddress);
-                startActivityForResult(intent, 1);
             }
         });
+    }
 
+    public void btDevicesView() {
+        Intent intent = new Intent(MainActivity.this, BtDevicesActivity.class);
+        startActivityForResult(intent, 1);
     }
 
     public void addComponents(){
@@ -89,40 +70,28 @@ public class MainActivity extends AppCompatActivity {
         textView1 = (TextView) findViewById(R.id.txt1);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         button = (Button) findViewById(R.id.button);
-        nazwa = new ArrayList<String>();
-        macaddress = new ArrayList<String>();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Snackbar.make(this.findViewById(R.id.action_settings), "Program wykonał Rafał Parafiniuk!", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                return true;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            if (pairedDevices.size() > 0) {
-                // There are paired devices. Get the name and address of each paired device.
-                for (BluetoothDevice device : pairedDevices) {
-                    String deviceName = device.getName();
-                    String deviceHardwareAddress = device.getAddress(); // MAC address
+            case R.id.action_bluetooth:
+                btDevicesView();
+                return true;
 
-                    textView1.setText(deviceName);
-                    textView2.setText(deviceHardwareAddress);
+            default:
+                return super.onOptionsItemSelected(item);
 
-                }
-            }
-            return true;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
